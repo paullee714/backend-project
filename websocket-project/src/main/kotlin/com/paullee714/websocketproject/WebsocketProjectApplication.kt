5d53d049bdd847609fb15eec597e3decf8c1ad28
paullee714 <paul_lee@kakao.com>
2024-08-05 23:01:39 +0900

    feat: init socket project

---
+++ websocket-project/src/main/kotlin/com/paullee714/websocketproject/WebsocketProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.paullee714.websocketproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketProjectApplication>(*args)
+}
---
+++ websocket-project/src/test/kotlin/com/paullee714/websocketproject/WebsocketProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.paullee714.websocketproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsocketProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
